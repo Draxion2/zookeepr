3987183a0415af3c66afcdde370229a466bf2c98
= <holdanmcmullen@gmail.com>
2022-04-01 01:35:09 -0600

    intergrated front-end

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 const { animals } = require("./data/animals.json");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -110,6 +113,21 @@ app.post("/api/animals", (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // no code below this!
 app.listen(PORT, () => {
